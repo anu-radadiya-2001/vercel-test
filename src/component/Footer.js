42998e8a9e3bef559c7de536e0aa5ed25a3c9325
Anu <64460527+anu-radadiya-2001@users.noreply.github.com>
2023-09-03 12:55:22 +0530

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -171,7 +171,7 @@ const Footer = () => {
     },
   ];
 
-  const { theme, setTheme } = useTheme(true);
+  const { theme, setTheme } = useTheme(false);
   
   const handleThemeChange = () => {
     if (theme === "light") {
